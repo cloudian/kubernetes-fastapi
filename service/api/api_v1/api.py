28e71d5e41805530653c1057f9864e087b42b3e6
Gary Ogasawara <garyo@cloudian.com>
2022-01-26 15:36:34 -0800

    Add lambda_handler.py

--- service/api/api_v1/api.py
+++ service/api/api_v1/api.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 
 from .endpoints.hello import router as triage_router
-
+from .endpoints.lambda_handler import router as lambda_router
 
 router = APIRouter()
 router.include_router(triage_router)
+router.include_router(lambda_router)
